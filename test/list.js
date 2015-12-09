f2f1acde92b1048833d5b07be9cbe2185cd5d6ce
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-12-09 14:49:45 +0000

    fix test

--- test/list.js
+++ test/list.js
@@ -1,6 +1,5 @@
 var test = require("tape");
 var ddata = require("../");
-var l = console.error;
 
 function makeOptions(data){
     return { data: { root: data }, hash: {} };
@@ -10,12 +9,26 @@ test("descendants", function(t){
     var options = makeOptions([
         { id: "one" }, { id: "two", memberof: "one" }, { id: "three", memberof: "two" }, { id: "four"}
     ]);
+    options.hash.min = 0
+
     t.deepEqual(ddata.descendants.call({ id: "one" }, options), [
         { id: 'two', memberof: 'one' }, { id: 'three', memberof: 'two' }
     ]);
+
+    var options = makeOptions([
+        { id: "one" }, { id: "two", memberof: "one" }, { id: "three", memberof: "two" }, { id: "four"}
+    ]);
+    options.hash.min = 0
+
     t.deepEqual(ddata.descendants.call({ id: "two" }, options), [
         { id: 'three', memberof: 'two' }
     ]);
+
+    var options = makeOptions([
+        { id: "one" }, { id: "two", memberof: "one" }, { id: "three", memberof: "two" }, { id: "four"}
+    ]);
+    options.hash.min = 0
+
     t.deepEqual(ddata.descendants.call({ id: "four" }, options), []);
     t.end();
 });
